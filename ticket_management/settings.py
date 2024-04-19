4cf82b3c64fa86161da06b9d138d8d135094a93a
jaguaru <jaguaru@outlook.com>
2024-04-19 13:43:00 -0600

    Adding to installed apps

--- ticket_management/settings.py
+++ ticket_management/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ticket_app',
+
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
