6ee17d718e467d25410edf97eec0ca2407fe32bd
jaguaru <jaguaru@outlook.com>
2024-04-20 15:14:39 -0600

    Changing order user_register and user_login in token.py

--- ticket_management/token.py
+++ ticket_management/token.py
@@ -13,20 +13,6 @@ from django.contrib.auth.models import User
 
 
 @api_view(['POST'])
-def user_login(request):
-
-    user = get_object_or_404(User, username=request.data['username'])
-
-    if not user.check_password(request.data['password']):
-        return Response({ "error": "Invalid password" }, status=status.HTTP_400_BAD_REQUEST)
-    
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserDataSerializer(instance=user)
-
-    return Response({ "token": token.key, "user": serializer.data }, status=status.HTTP_200_OK)
-
-
-@api_view(['POST'])
 def user_register(request):
 
     serializer = UserRegisterSerializer(data=request.data)
@@ -43,6 +29,20 @@ def user_register(request):
         return Response({ 'token': token.key, "user": serializer.data }, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def user_login(request):
+
+    user = get_object_or_404(User, username=request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response({ "error": "Invalid password" }, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserDataSerializer(instance=user)
+
+    return Response({ "token": token.key, "user": serializer.data }, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
