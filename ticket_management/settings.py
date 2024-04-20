aed33c324a05a8676fcfbda504dc5826454ce0d6
jaguaru <jaguaru@outlook.com>
2024-04-19 18:32:03 -0600

    Adding dotenv and variables to server and database in setting.py

--- ticket_management/settings.py
+++ ticket_management/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from dotenv import dotenv_values
+get_env_var = dotenv_values()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ve@w86!_jm573do%&12anb3u1h-q&3)mck1d2u20as_kl*n$r'
+SECRET_KEY = get_env_var['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'ticket_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_var['NAME'],
+        'USER': get_env_var['USER'],
+        'PASSWORD': get_env_var['PASS'],
+        'HOST': get_env_var['HOST'],
+        'PORT': int(get_env_var['PORT']),
     }
 }
 
