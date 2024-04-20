f6c09e3d431a9fa52de59093bb47df03962ad999
jaguaru <jaguaru@outlook.com>
2024-04-19 19:50:52 -0600

    Adding TokenAuthentication, authentication_classes, permissions_classes and IsAuthenticated to token.py

--- ticket_management/token.py
+++ ticket_management/token.py
@@ -2,7 +2,10 @@ from .serializers import UserSerializer
 
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from django.shortcuts import get_object_or_404
