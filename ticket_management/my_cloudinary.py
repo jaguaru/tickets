1425d98e866110e5b2b2e765895dd95dfad2a296
jaguaru <jaguaru@outlook.com>
2024-04-19 20:22:57 -0600

    Adding save_cloud_image and save_cloud_image_wait to my_cloudinary.py

---
+++ ticket_management/my_cloudinary.py
@@ -0,0 +1,34 @@
+from dotenv import dotenv_values
+get_env_var = dotenv_values()
+
+from io import BytesIO
+
+from celery import shared_task
+
+import cloudinary
+
+cloudinary.config( 
+    cloud_name = get_env_var['CLOUD_NAME'],
+    api_key = get_env_var['API_KEY'],
+    api_secret = get_env_var['API_SECRET'],
+    # secure = get_env_var['SECURE']
+)
+
+import cloudinary.uploader
+import cloudinary.api
+
+
+@shared_task
+def save_cloud_image(image_file):
+    try:
+        with BytesIO(image_bytes) as image_file:
+            return cloudinary.uploader.upload(image_file)
+
+    except Exception as _except:
+        return str(_except)
+
+
+@shared_task
+def save_cloud_image_wait(image_file):
+    cloud_image_saved = save_cloud_image.apply_async(args=[image_file])
+    return cloud_image_saved.wait()
