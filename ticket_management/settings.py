758fcc3d7eba90d3a6bf2a95f3717c15e0990c17
jaguaru <jaguaru@outlook.com>
2024-04-19 18:32:48 -0600

    Adding dotenv and variables to server and database in setting.py

--- ticket_management/settings.py
+++ ticket_management/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_env_var['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = get_env_var['DEBUG']
 
 ALLOWED_HOSTS = []
 
