d367d85cc01f3c7525cfce6ba130fc209017add1
jaguaru <jaguaru@outlook.com>
2024-04-19 23:25:50 -0600

    Adding TicketSerializer and TicketImageSerializer to serializers.py

--- ticket_management/serializers.py
+++ ticket_management/serializers.py
@@ -14,8 +14,14 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = ['id', 'image_name', 'image_url', 'uploaded_at']
 
-
 class TicketSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ticket
+        fields = ['id', 'num_images', 'user', 'status', 'created_at', 'images']
+
+
+class TicketImageSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True)
 
     class Meta:
