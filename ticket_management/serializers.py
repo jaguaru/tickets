0cbf4e407d2e2875f48a11c7ccb3a05218181dca
jaguaru <jaguaru@outlook.com>
2024-04-19 19:28:06 -0600

    Adding UserSerializer, ImageSeralizer and TicketSerializer to serializers.py

---
+++ ticket_management/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Ticket, Image
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['id', 'image_name', 'image_url', 'uploaded_at']
+        #fields = '__all__'
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = ['id', 'num_images', 'user', 'status', 'created_at', 'images']
+        #fields = '__all__'
