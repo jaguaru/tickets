5622f9deb825e9ad2255a48c3c24e8238c1e7eac
jaguaru <jaguaru@outlook.com>
2024-04-19 19:36:52 -0600

    Adding ticket_management, rest_framework and authtoken to settings.py

--- ticket_management/settings.py
+++ ticket_management/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ticket_management',
+    
+    'rest_framework',
+    'rest_framework.authtoken',
+
 ]
 
 MIDDLEWARE = [
