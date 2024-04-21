b6ddda2438ecffad29ec7c6f51472be1c7a223d8
jaguaru <jaguaru@outlook.com>
2024-04-21 14:10:10 -0600

    Changing the message language in the error except response in monitoring_ticket_status in views.py

--- ticket_management/views.py
+++ ticket_management/views.py
@@ -50,7 +50,7 @@ def monitoring_ticket_status(request):
             return Response(ticket_serializer.data)
 
         except Ticket.DoesNotExist:
-            return Response({ 'error': 'Ticket no encontrado!' }, status=status.HTTP_404_NOT_FOUND)
+            return Response({ 'error': 'The ticket does not exist or has not been found!' }, status=status.HTTP_404_NOT_FOUND)
     
     else:
         return Response({ 'error': 'No puedes usar este ticket!' }, status=status.HTTP_401_UNAUTHORIZED)
